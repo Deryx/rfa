85d67b0b9bbc110f3934121e05ecd380f4cc26f2
Deryx E. Scott <deryxescott@gmail.com>
2016-05-12 00:29:55 -0400

    Main.css edit

--- scripts/application.js
+++ scripts/application.js
@@ -2,13 +2,13 @@ $(function() {
 	$( 'body' ).showlightbox();
 	$( '#sc-lightbox' ).fadeIn();
 
-	$('header > nav > ul > li > a[href^="#"]').on('click', function(e) {
+	$( 'header > nav > ul > li > a[href^="#"]' ).on('click', function(e) {
 		e.preventDefault();
 
 		var target = this.hash;
 		$target = $(target);
 
-		$('html, body').stop().animate({
+		$( 'html, body' ).stop().animate({
 			'scrollTop': $target.offset().top
 		}, 900, 'swing', function() {
 			window.location.hash = target;
